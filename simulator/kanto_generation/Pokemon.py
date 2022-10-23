63c25faa216b74ebebd328de55eb5c15abc55b64
ElvisRodriguez <elvisrodriguez1992@gmail.com>
2022-10-23 01:06:18 -0400

    Added getters/setters for initilizable attrs

--- simulator/kanto_generation/Pokemon.py
+++ simulator/kanto_generation/Pokemon.py
@@ -1,20 +1,30 @@
 import calculations
-import constants
+from simulator.constants import (
+    BaseStats,
+    DynamicValues,
+    EffortValues,
+    ExperienceGroup,
+    MAX_LEVEL,
+    Stat,
+    Stats,
+    Status,
+    Types,
+)
 from Technique import Technique
 
 
 class Pokemon:
     def __init__(
         self,
-        base_stats: constants.BaseStats,
-        dynamic_values: constants.DynamicValues,
-        effort_values: constants.EffortValues,
-        experience_group: constants.ExperienceGroup,
+        base_stats: BaseStats,
+        dynamic_values: DynamicValues,
+        effort_values: EffortValues,
+        experience_group: ExperienceGroup,
         level: int,
         moveset: list[Technique],
         name: str,
-        status: constants.Status,
-        types: tuple[constants.Type],
+        status: Status,
+        types: Types,
     ) -> None:
         self._base_stats = base_stats
         self._dynamic_values = dynamic_values
@@ -28,28 +38,59 @@ class Pokemon:
         self._experience = calculations.get_experience(experience_group, level)
     
     @property
-    def base_stats(self) -> constants.BaseStats:
+    def base_stats(self) -> BaseStats:
         return self._base_stats
     
     @property
-    def dynamic_values(self) -> constants.DynamicValues:
+    def dynamic_values(self) -> DynamicValues:
         return self._dynamic_values
     
     @property
-    def effort_values(self) -> constants.EffortValues:
+    def effort_values(self) -> EffortValues:
         return self._effort_values
     
     @effort_values.setter
-    def effort_values(self, stat: constants.Stat, experience_gained: int) -> None:
-        pass
+    def effort_values(self, stat: Stat, effort_value_points: int) -> None:
+        self._effort_values[stat] += effort_value_points
+    
+    @property
+    def experience_group(self) -> ExperienceGroup:
+        return self._experience_group
+    
+    @property
+    def level(self) -> int:
+        return self._level
+    
+    @property
+    def moveset(self) -> list[Technique]:
+        return self._moveset
 
     @property
     def name(self) -> str:
         return self._name
     
     @property
-    def level(self) -> int:
-        return self._level
+    def status(self) -> Status:
+        return self._status
     
+    @status.setter
+    def status(self, new_status: Status) -> None:
+        self._status = new_status
     
+    @property
+    def types(self) -> Types:
+        return self._types
     
+    @property
+    def experience(self) -> int:
+        return self._experience
+    
+    @experience.setter
+    def experience(self, _experience: int) -> None:
+        if self._level < MAX_LEVEL:
+            self._experience += _experience
+            next_level_experience = calculations.get_experience(self._experience_group, self._level + 1)
+            while next_level_experience < self._experience:
+                self._level += 1
+                next_level_experience = calculations.get_experience(self._experience_group, self._level)
+            self._level = min(self._level, MAX_LEVEL)
