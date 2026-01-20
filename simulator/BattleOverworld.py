76b3e2b410b4ab13a16cbd9a1bd8b8456ec8028e
Elvis Rodriguez <elvisrodriguez1992@gmail.com>
2026-01-19 22:21:33 -0500

    Added more moves to the overworld

--- simulator/BattleOverworld.py
+++ simulator/BattleOverworld.py
@@ -1,9 +1,30 @@
+from constants import Technique
+
 class BattleOverworld:
     def __init__(self):
+        self._fury_cutter = 0
+        self._rage = 0
         self._rain_dance = 0
         self._rollout = 0
         self._sunny_day = 0
+        self._moves_used = []
     
+    def reset(self):
+        self._fury_cutter = 0
+        self._rage = 0
+        self._rain_dance = 0
+        self._rollout = 0
+        self._sunny_day = 0
+        self._moves_used = []
+    
+    @property
+    def fury_cutter(self) -> int:
+        return self._fury_cutter
+
+    @property
+    def rage(self) -> int:
+        return self._rage
+
     @property
     def rain_dance(self) -> int:
         return self._rain_dance
@@ -15,3 +36,13 @@ class BattleOverworld:
     @property
     def sunny_day(self) -> int:
         return self._sunny_day
+    
+    @property
+    def moves_used(self) -> list:
+        return self._moves_used
+    
+    @property
+    def last_move_used(self) -> Technique:
+        if len(self._moves_used) >= 1:
+            return self._moves_used[-1]
+        return None
