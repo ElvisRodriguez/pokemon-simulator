d888f936982425c27401afadfeea62025db8615f
Elvis Rodriguez <elvisrodriguez1992@gmail.com>
2026-02-17 12:27:06 -0500

    Added turn by turn damage logic for bind; will reuse for similar moves

--- simulator/BattleOverworld.py
+++ simulator/BattleOverworld.py
@@ -1,3 +1,5 @@
+import random
+
 from constants import Technique
 import moves
 from Pokemon import Pokemon
@@ -18,6 +20,17 @@ class BattleOverworld:
             return self.moves_used[-1]
         return None
     
-    def reset_pokemon(pokemon: Pokemon):
+    def reset_pokemon(self, pokemon: Pokemon):
         pokemon.stats = pokemon._frozen_stats
         pokemon.stat_stages.reset()
+    
+    def bind(self, BIND: Technique, defender: Pokemon):
+        if BIND.turns == 0:
+            return
+        damage = defender.max_hp / 8
+        if BIND.turns < 5:
+            defender.stats.HP -= damage
+        if BIND.turns == 5:
+            if random.random() < 1/2:
+                defender.stats.HP -= damage
+            BIND.turns = 0
